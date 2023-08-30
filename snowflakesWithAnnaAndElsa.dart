712d9a3772cac4aeed92e95c8bfb5d333e45797c
Janaina <jmtannus@gmail.com>
2023-08-30 10:50:41 -0300

    melhorando o angulo do loop de 90 graus

--- snowflakesWithAnnaAndElsa.dart
+++ snowflakesWithAnnaAndElsa.dart
@@ -1,4 +1,4 @@
-turnLeft(15);
+turnRight(45);
 for (var count3 = 0; count3 < 6; count3++) {
   for (var count = 0; count < 3; count++) {
       turnLeft(45);
@@ -13,16 +13,12 @@ for (var count3 = 0; count3 < 6; count3++) {
   turnLeft(60);
   jumpTo(13);
 }
+turnRight(15);
 for (var count5 = 0; count5 < 6; count5++) {
   for (var count4 = 0; count4 < 4; count4++) {
       moveForward(50);
     turnRight(90);
   }
+  turnLeft(60);
+  jumpTo(13);
 }
-
-/*
-for (var count6 = 0; count6 < 3; count6++) {
-}
-turnLeft(45);
-jumpTo(13);
-*/
