7041c264b35a44bbec32698cdf1652cc3c57fc76
Janaina <jmtannus@gmail.com>
2023-08-30 09:43:35 -0300

    acrescentando um novo loop

--- snowflakesWithAnnaAndElsa.dart
+++ snowflakesWithAnnaAndElsa.dart
@@ -1,4 +1,9 @@
-for (var count = 0; count < 6; count++) {
-  moveForward(50);
-  turnRight(60);
+for (var count2 = 0; count2 < 6; count2++) {
+    for (var count = 0; count < 6; count++) {
+    moveForward(50);
+    turnRight(60);
+    }
+    turnRight(60);
+    moveForward(150);
+    jumpTo(13);
 }
