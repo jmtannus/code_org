81c08e3d36f09be1838da214f1763147e64f4273
Janaina <jmtannus@gmail.com>
2023-08-30 10:13:11 -0300

    acrescentando mais uma série de loop

--- snowflakesWithAnnaAndElsa.dart
+++ snowflakesWithAnnaAndElsa.dart
@@ -1,9 +1,13 @@
-for (var count2 = 0; count2 < 6; count2++) {
-    for (var count = 0; count < 6; count++) {
-    moveForward(50);
+for (var count3 = 0; count3 < 6; count3++) {
+  for (var count = 0; count < 6; count++) {
+      moveForward(50);
     turnRight(60);
-    }
-    turnRight(60);
-    moveForward(150);
-    jumpTo(13);
+  }
+  turnRight(60);
+  moveForward(150);
+  for (var count2 = 0; count2 < 4; count2++) {
+      moveForward(15);
+    turnLeft(90);
+  }
+  jumpTo(13);
 }
